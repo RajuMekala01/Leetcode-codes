8bf3686e78c4afac4fa4f123073d5bb03c0a3dd5
RAJU MEKALA <141472950+RajuMekala01@users.noreply.github.com>
2026-09-04 22:38:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (70.76%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int res=0;
+        for(int i=0;i<stones.length();i++){
+            char ch=stones.charAt(i);
+            for(int j=0;j<jewels.length();j++){
+                char ch1=jewels.charAt(j);
+                if(ch==ch1){
+                    res++;
+                    break;
+                }
+            }
+        }
+        return res;
+        
+    }
+}
