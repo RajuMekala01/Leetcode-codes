1c612054c7212c1d21d72fcf3b47dd2f262fce7f
RAJU MEKALA <141472950+RajuMekala01@users.noreply.github.com>
2026-09-06 23:39:06 +0530

    Time: 1 ms (99.95%), Space: 93.8 MB (96.53%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int min=1000000;
+        int max=0;
+        for(int i=0;i<n;i++){
+            min=Math.min(min,prices[i]);
+            max=Math.max(max,prices[i]-min);
+        }
+        return max;
+    }
+}
